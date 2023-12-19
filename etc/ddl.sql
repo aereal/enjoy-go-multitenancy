93e20287b4a083ed3891a6736691f84463fafe43
aereal <aereal@aereal.org>
2023-12-19 22:00:07 +0900

    feat: update DDL

--- etc/ddl.sql
+++ etc/ddl.sql
@@ -1,3 +1,25 @@
+create database tenant_1;
+
+use tenant_1;
+
+create table if not exists users (
+  id char(20) character set ascii primary key,
+  name varchar(255) not null
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 collate=utf8mb4_unicode_ci;
+
+create database tenant_2;
+
+use tenant_2;
+
+create table if not exists users (
+  id char(20) character set ascii primary key,
+  name varchar(255) not null
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 collate=utf8mb4_unicode_ci;
+
+create database tenant_3;
+
+use tenant_3;
+
 create table if not exists users (
   id char(20) character set ascii primary key,
   name varchar(255) not null
